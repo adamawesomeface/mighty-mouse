5b087ab01befc97fed2075d204243c82ee8d0783
adamawesomeface <adam.stogdill@gmail.com>
2018-09-04 11:33:03 -0400

    adding everything back in

---
+++ src/mighty-mouse.php
@@ -0,0 +1,60 @@
+<?php
+  class MightyMouse {
+    public $data   = array();
+    public $fields = array();
+    function __construct() {
+      $this->data = self::getData();
+      print_r($this->data);
+    }
+    public function getData(){
+      $jsonData = stripslashes(file_get_contents(__DIR__ . '/data/data.json'));
+      $jsonData = rtrim($jsonData, "\0");
+      $aData = json_decode($jsonData, true);
+      return $aData;
+    }
+    public function setData($newData){
+      $fp = fopen('data/data.json', 'w');
+      fwrite($fp, json_encode($newData));
+      fclose($fp);
+      return self::getData();
+    }
+    public function getFields(){
+      $fields = file_get_contents(__DIR__ . '/data/fields.json');
+      $fields = json_decode($fields, true);
+      return $fields;
+    }
+    public function setFields($newFields){
+      $fp = fopen(__DIR__ . '/data/fields.json', 'w');
+      fwrite($fp, json_encode($newFields));
+      fclose($fp);
+      return self::getFields();
+    }
+
+    // GETTING DATA OUT
+    public function getFieldValue($path, $default){
+      if(!empty($this->data)){
+        return self::getArrayByPath($this->data, $path);
+      } else {
+        return $default;
+      }
+    }
+    public function value($key, $default){   // Sugar
+      return self::getFieldValue($key, $default);
+    }
+
+    // UTILITIES
+    public function getArrayByPath(&$arr, $path, $separator='.') {
+      $keys = explode($separator, $path);
+      foreach ($keys as $key) {
+        $arr = &$arr[$key];
+      }
+      return $arr;
+    }
+    public function setArrayByPath(&$arr, $path, $value, $separator='.') {
+      $keys = explode($separator, $path);
+      foreach ($keys as $key) {
+        $arr = &$arr[$key];
+      }
+      $arr = $value;
+    }
+  }
